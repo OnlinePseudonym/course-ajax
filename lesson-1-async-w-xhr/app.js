21f6abe3d6bf4f881a210126e43df0f6ea9234fa
Kevin Reynolds <Kevin.T.Reynolds30@gmail.com>
2017-10-02 23:32:17 -0700

    finished all sections

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -8,5 +8,57 @@
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+
+        const imgRequest = new XMLHttpRequest();
+        imgRequest.onload = addImage;
+        imgRequest.onerror = function (err) {
+            requestError(err, 'image');
+        };
+        imgRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
+        imgRequest.setRequestHeader('Authorization', 'Client-ID c2a8dc48e99f388612aaceab726995bfb362915795f0a63527a37c9dfef7c1dd');
+        imgRequest.send();
+
+        const articleRequest = new XMLHttpRequest();
+        articleRequest.onload = addArticles;
+        articleRequest.onerror = function (err) {
+          requestError(err, 'articles');
+        };
+        articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=36c120a7a4c347449c9d34bb0885795b`);
+        articleRequest.send();
+
+        function addImage() {
+            let htmlContent = '';
+            const data = JSON.parse(this.responseText);
+
+            if (data && data.results && data.results[0]){
+                const firstImage = data.results[0];
+                htmlContent = `<figure>
+                    <img src="${firstImage.urls.regular}" alt="${searchedForText}">
+                    <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+                </figure>`;
+            } else {
+              htmlContent = '<div class="error-no-image">No images available</div>';
+            }
+
+            responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+        }
+
+
+        function addArticles() {
+          let htmlContent = '';
+          const data = JSON.parse(this.responseText);
+
+          if (data.response && data.response.docs && data.response.docs.length > 1) {
+              htmlContent = '<ul>' + data.response.docs.map(article => `<li class="article">
+                      <h2><a href="${article.web_url}">${article.headline.main}</a></h2>
+                      <p>${article.snippet}</p>
+                  </li>`
+              ).join('') + '</ul>';
+          } else {
+            htmlContent = '<div class="error-no-articles">No articles available</div>';
+          }
+
+          responseContainer.insertAdjacentHTML('beforeend', htmlContent);
+        }
     });
 })();
--- lesson-2-async-w-jQuery/app.js
+++ lesson-2-async-w-jQuery/app.js
@@ -10,5 +10,58 @@
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+
+        $.ajax({
+            url: `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`,
+            headers: {
+                Authorization: 'Client-ID c2a8dc48e99f388612aaceab726995bfb362915795f0a63527a37c9dfef7c1dd'
+            }
+        }).done(addImage)
+        .fail(function(err) {
+          requestError(err, 'image');
+        });
+
+        $.ajax({
+            url: `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=36c120a7a4c347449c9d34bb0885795b`
+        }).done(addArticles)
+        .fail(function(err) P{
+          requestError(err, 'articles');
+        });
+
+        function addImage(images) {
+            let htmlContent = '';
+
+            if (images && images.results && images.results[0]){
+                const firstImage = images.results[0];
+                htmlContent = `<figure>
+                    <img src="${firstImage.urls.regular}" alt="${searchedForText}">
+                    <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+                </figure>`;
+            } else {
+              htmlContent = '<div class="error-no-image">No images available</div>';
+            }
+
+            responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+        }
+
+        function addArticles(articles) {
+            let htmlContent = '';
+
+            if (articles.response && articles.response.docs && articles.response.docs.length > 1) {
+                htmlContent = '<ul>' + articles.response.docs.map(article => `<li class="article">
+                        <h2><a href="${article.web_url}">${article.headline.main}</a></h2>
+                        <p>${article.snippet}</p>
+                    </li>`
+                ).join('') + '</ul>';
+            } else {
+                htmlContent = '<div class="error-no-articles">No articles available</div>';
+            }
+            responseContainer.insertAdjacentHTML('beforeend', htmlContent);
+        }
+
+        function requestError(e, part) {
+            console.log(e);
+            responseContainer.insertAdjacentHTML('beforeend', `<p class="network-warning error">Error</p>"`);
+        }
     });
 })();
